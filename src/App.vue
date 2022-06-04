3a025a2b12f19b069d49a29685be503143cfb620
Uriel1621 <uriel.fischman@gmail.com>
2022-06-04 17:23:05 -0300

    Se agrega la API de peliculas y la busqueda por nombre

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchMovies/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchMovies from './components/SearchMovies.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchMovies
   }
 }
 </script>
@@ -23,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
 }
 </style>
